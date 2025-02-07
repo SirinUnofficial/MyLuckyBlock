51bd32acc2dc16faebaed228059f04ef09602de4
Syca <64587684+Sycamore0@users.noreply.github.com>
2025-02-07 17:17:09 +0800

    [SRC]Remove tooltip

--- src/main/java/io/github/sycamore0/myluckyblock/block/ModBlocks.java
+++ src/main/java/io/github/sycamore0/myluckyblock/block/ModBlocks.java
@@ -5,7 +5,6 @@ import net.minecraft.block.*;
 import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.function.Function;
@@ -19,7 +18,7 @@ public class ModBlocks {
 
     static {
         MY_LUCKY_BLOCK = register("my_lucky_block", LuckyBlock::new, Block.Settings.create().mapColor(MapColor.PINK).strength(0.5f).resistance(5000000.0f));
-        DEBUG_LUCKY_BLOCK = register("debug_lucky_block", (settings) -> new LuckyBlock(settings, MyLuckyBlock.MOD_ID, true, Text.translatable("block.myluckyblock.debug_lucky_block.tooltip")), Block.Settings.create().mapColor(MapColor.GOLD).strength(0.5f).resistance(5000000.0f));
+        DEBUG_LUCKY_BLOCK = register("debug_lucky_block", (settings) -> new LuckyBlock(settings, MyLuckyBlock.MOD_ID, true), Block.Settings.create().mapColor(MapColor.GOLD).strength(0.5f).resistance(5000000.0f));
     }
 
     private static Block register(String path, Function<Block.Settings, Block> factory, Block.Settings settings) {
