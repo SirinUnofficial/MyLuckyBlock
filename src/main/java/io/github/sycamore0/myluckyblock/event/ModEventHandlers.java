8664466de0c706eedf9a3ead619cee2a9980fe52
Syca <64587684+Sycamore0@users.noreply.github.com>
2025-03-04 18:23:03 +0800

    Delete all

--- src/main/java/io/github/sycamore0/myluckyblock/event/ModEventHandlers.java
+++
@@ -1,9 +0,0 @@
-package io.github.sycamore0.myluckyblock.event;
-
-import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
-
-public class ModEventHandlers {
-    public static void onInitialize() {
-        PlayerBlockBreakEvents.AFTER.register((world, player, pos, state, blockEntity) -> BreakLuckyBlock.breakLuckyBlock(world, player, pos, state));
-    }
-}
