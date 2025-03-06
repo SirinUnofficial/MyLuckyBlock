85d186f11b4b6113b15e1f1c751b0b2e584af291
Syca <64587684+Sycamore0@users.noreply.github.com>
2025-03-06 18:05:18 +0800

    Del Forge

--- README.md
+++ README.md
@@ -3,16 +3,6 @@
 为Minecraft 1.21.X添加了一个幸运方块，玩家可以破坏它以触发随机事件。<br>
 Add a lucky block to Minecraft 1.21.X that players can break to exec random events.<br>
 
-## 依赖Dependencies
-| Support Version | Fabric Loader | Fabric API |
-| -------- |---------------| -------- |
-| 1.21-1.21.1 | `>=0.15.11`   | `>=0.102.1` |
-| 1.21.2-1.21.3 | `>=0.16.7`      | `>=0.106.1` |
-| 1.21.4 | `>=0.16.9`      | `>=0.111.0` |
-| 1.21.5 Snapshot | `>=0.16.10`     | `>=0.115.2` |
-> `1.21.5`仅在`25w05a`上测试过<br>
-> `1.21.5` has been tested on `25w05a` only<br>
-
 # 注意Attention
 > Project may not be English friendly.<br>
 
--- common/src/main/java/io/github/sycamore0/myluckyblock/CommonClass.java
+++ common/src/main/java/io/github/sycamore0/myluckyblock/CommonClass.java
@@ -8,10 +8,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// This class is part of the common project meaning it is shared between all supported loaders. Code written here can only
-// import and access the vanilla codebase, libraries used by vanilla, and optionally third party libraries that provide
-// common compatible binaries. This means common code can not directly use loader specific concepts such as Forge events
-// however it will be compatible with all supported mod loaders.
 public class CommonClass {
     public static final Map<String, List<JsonObject>> loadedEventsByMod = new HashMap<>();
     public static final List<String> modIdList = new ArrayList<>();
@@ -21,11 +17,6 @@ public class CommonClass {
     }
 
     public static void init() {
-        // It is common for all supported loaders to provide a similar feature that can not be used directly in the
-        // common code. A popular way to get around this is using Java's built-in service loader feature to create
-        // your own abstraction layer. You can learn more about this in our provided services class. In this example
-        // we have an interface in the common code and use a loader specific implementation to delegate our call to
-        // the platform specific approach.
         if (Services.PLATFORM.isModLoaded(Constants.MOD_ID)) {
             Constants.LOG.info("Hello World!");
         }
