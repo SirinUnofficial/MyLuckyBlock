b805a6c5cdb81601bfb607a0de8697ef68287f96
Syca <64587684+Sycamore0@users.noreply.github.com>
2025-03-12 14:42:01 +0800

    [Fabric]Easy create block by createNewLB

--- fabric/src/main/java/io/github/sycamore0/myluckyblock/block/ModBlocks.java
+++ fabric/src/main/java/io/github/sycamore0/myluckyblock/block/ModBlocks.java
@@ -14,13 +14,28 @@ public class ModBlocks {
     public static final Block MY_LUCKY_BLOCK;
 
     static {
-        MY_LUCKY_BLOCK = register("my_lucky_block", new LuckyBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_PINK).strength(0.5f).explosionResistance(5000000.0f)));
+        MY_LUCKY_BLOCK = createNewLB("my_lucky_block", Constants.MOD_ID, true);
+
+        // Old Method
+        // MY_LUCKY_BLOCK = register("my_lucky_block", new LuckyBlock(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_PINK).strength(0.5f).explosionResistance(5000000.0f)));
     }
 
-    public static <T extends Block> T register(String path, T block) {
-        Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, path), block);
-        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, path), new BlockItem(block, new Item.Properties()));
+    private static <T extends Block> T register(String path, T block, String modId) {
+        Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(modId, path), block);
+        Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(modId, path), new BlockItem(block, new Item.Properties()));
         return block;
+    }
+
+    public static Block createNewLB(String blockId, String modId) {
+        return createNewLB(blockId, modId, MapColor.COLOR_PINK, false);
+    }
+
+    public static Block createNewLB(String blockId, String modId, boolean includeBuiltIn) {
+        return createNewLB(blockId, modId, MapColor.COLOR_PINK, includeBuiltIn);
+    }
+
+    public static Block createNewLB(String blockId, String modId, MapColor color, boolean includeBuiltIn) {
+        return register(blockId, new LuckyBlock(BlockBehaviour.Properties.of().mapColor(color).strength(0.5f).explosionResistance(5000000.0f), modId, includeBuiltIn), modId);
     }
 
     public static void registerModBlocks() {
