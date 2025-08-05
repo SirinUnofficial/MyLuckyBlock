23fb2776a5e8a4f16bd3e5a8f79a0ede2118bb10
Syca <64587684+Sycamore0@users.noreply.github.com>
2025-08-05 23:47:40 +0800

    [NeoForge]Support Config

--- neoforge/src/main/java/io/github/sycamore0/myluckyblock/MyLuckyBlock.java
+++ neoforge/src/main/java/io/github/sycamore0/myluckyblock/MyLuckyBlock.java
@@ -1,18 +1,29 @@
 package io.github.sycamore0.myluckyblock;
 
 import io.github.sycamore0.myluckyblock.block.ModBlocks;
+import io.github.sycamore0.myluckyblock.config.ModConfig;
+import io.github.sycamore0.myluckyblock.config.ModConfigManager;
 import io.github.sycamore0.myluckyblock.event.ModEventHandlers;
 import io.github.sycamore0.myluckyblock.item.ModItemGroups;
+import io.github.sycamore0.myluckyblock.screen.ConfigScreen;
+import io.github.sycamore0.myluckyblock.worldgen.ModFeatures;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(Constants.MOD_ID)
 public class MyLuckyBlock {
-    public MyLuckyBlock(IEventBus eventBus) {
+    public MyLuckyBlock(IEventBus eventBus, ModContainer container) {
         CommonClass.init();
+
+        ModConfig config = ModConfigManager.loadConfig();
+        container.registerExtensionPoint(IConfigScreenFactory.class, (MC, parent) -> new ConfigScreen(parent, config));
+
         ModBlocks.onInitialize(eventBus);
         ModItemGroups.onInitialize(eventBus);
         NeoForge.EVENT_BUS.register(ModEventHandlers.class);
+        ModFeatures.onInitialize();
     }
 }
