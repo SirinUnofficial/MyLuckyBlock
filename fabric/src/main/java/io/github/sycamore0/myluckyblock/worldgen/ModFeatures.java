1260ea8ed3bcd3a8b4fecc36ed103583cb0557cc
Syca <64587684+Sycamore0@users.noreply.github.com>
2025-08-05 17:15:40 +0800

    [Fabric]Support lb_gen tag

--- fabric/src/main/java/io/github/sycamore0/myluckyblock/worldgen/ModFeatures.java
+++ fabric/src/main/java/io/github/sycamore0/myluckyblock/worldgen/ModFeatures.java
@@ -2,16 +2,19 @@ package io.github.sycamore0.myluckyblock.worldgen;
 
 import io.github.sycamore0.myluckyblock.Constants;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.levelgen.GenerationStep;
 
 public class ModFeatures {
+    public static TagKey<Biome> LB_GEN_BIOME_TAG = TagKey.create(Registries.BIOME, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "lb_gen"));
+
     public static void onInitialize() {
         BiomeModifications.addFeature(
-                BiomeSelectors.all(),
+                biomeSelectionContext -> biomeSelectionContext.hasTag(LB_GEN_BIOME_TAG),
                 GenerationStep.Decoration.SURFACE_STRUCTURES,
                 ResourceKey.create(Registries.PLACED_FEATURE,
                         ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID, "lucky_block_surface"))
